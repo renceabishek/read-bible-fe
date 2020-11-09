7e2c5abfa05bb81541b5f2b0df0b1c5fa3146684
Rence Abishek <renceabishek@gmail.com>
2020-11-09 19:10:10 +0530

    adding main service

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -8,7 +8,7 @@ import { Profile } from '../model/Profile';
 })
 export class AuthenticationService {
 
-  private readonly URL ="http://localhost:8082/";
+  private readonly URL ="https://read-bible-service.herokuapp.com/";
 
   constructor(private http: HttpClient) { }
 
